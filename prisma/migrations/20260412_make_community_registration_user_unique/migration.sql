62f18b69a18efa86a3ca7fd1feefc0f19422840b
Tylordev <tylordeveloper@gmail.com>
2026-04-12 14:44:35 -0300

    Fix community registration uniqueness migration

--- prisma/migrations/20260412_make_community_registration_user_unique/migration.sql
+++ prisma/migrations/20260412_make_community_registration_user_unique/migration.sql
@@ -1,2 +1,17 @@
-ALTER TABLE "CommunityRegistrationEntry"
-ADD CONSTRAINT "CommunityRegistrationEntry_userId_key" UNIQUE ("userId");
+TRUNCATE TABLE
+  "CommunityRegistrationEntry",
+  "CommunityRegistrationBatch",
+  "CommunityRegistrationBoard"
+RESTART IDENTITY CASCADE;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_constraint
+    WHERE conname = 'CommunityRegistrationEntry_userId_key'
+  ) THEN
+    ALTER TABLE "CommunityRegistrationEntry"
+    ADD CONSTRAINT "CommunityRegistrationEntry_userId_key" UNIQUE ("userId");
+  END IF;
+END $$;
